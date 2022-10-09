7ac5c6c01cc566456fe71efa475ffda414bd14e3
Andreas-Hjort <73465555+Andreas-Hjort@users.noreply.github.com>
2022-10-09 17:31:56 +0200

    create show and movie

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -4,6 +4,7 @@ import { sanitizeStringWithTableRows } from "../../utils.js"
 export function initMovies(){
     document.getElementById("tbl-body").onclick = showAllMovies
     fetchAllMovies()
+    addOne()
 }
 
 export async function fetchAllMovies(){
@@ -27,6 +28,32 @@ function showAllMovies(data){
         const tableRowsString = tableRows.join("\n")
         document.getElementById("tbl-body").innerHTML = sanitizeStringWithTableRows(tableRowsString)
 }
+
+function addOne() {
+    document.getElementById("bnt-submit-movie").onclick = makeNewMovie
+    function makeNewMovie() {
+      const newMovie = {}
+      newMovie.title = document.getElementById("input-title").value
+      newMovie.genre = document.getElementById("input-genre").value
+      newMovie.ageLimit = document.getElementById("input-ageLimit").value
+      newMovie.productionYear = document.getElementById("input-productionYear").value
+      newMovie.runningTime = document.getElementById("input-runningTime").value
+
+
+      //Now newshow contains all required fields (MUST match the DTO on the backend) and their values
+
+      //Build the options object requred for a POST
+      const options = {}
+      options.method = "POST"
+      options.headers = { "Content-type": "application/json" }
+      options.body = JSON.stringify(newMovie)
+
+      fetch(URL, options)
+        .then(r => r.json())
+        .then(addedmovie => document.getElementById("returned-new-movie").innerText = JSON.stringify(addedmovie, null, 2)
+        )
+    }
+  }
 
 //  <td>
 //<--!<button> id="${movie.id}-column-id" type="button" skal lige færdigøres-->
