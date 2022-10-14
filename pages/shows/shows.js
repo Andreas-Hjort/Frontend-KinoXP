97a2110d01b65aee9a17bd42c743bf0ca0354b75
Andreas-Hjort <73465555+Andreas-Hjort@users.noreply.github.com>
2022-10-14 03:36:26 +0200

    Alot of css and connecting backend to dropdown inputs for modals

--- pages/shows/shows.js
+++ pages/shows/shows.js
@@ -1,11 +1,13 @@
 //const URL = "https://kinoxp.azurewebsites.net/shows/"
 const URL = "http://localhost:8080/shows/"
+
 import { sanitizeStringWithTableRows } from "../../utils.js"
+import { fetchMovieToEdit } from "../movies/movies.js"
 
 export function initShows() {
     fetchAllShows()
-    addOne()
     document.getElementById("tbl-body").onclick = editTarget
+    document.getElementById("add-show").onclick = addShow
 }
 
 
@@ -20,14 +22,14 @@ function showAllShows(data) {
         <td>${show.movie.title}</td>
         <td>${show.movie.genre}</td>
         <td>${show.theater}</td>
+        <td>${show.movie.runningTime}</td>
         <td>${show.showingTime}</td>
         <td>${show.date}
-        <td>${show.movie.runningTime}</td>
         <td>
-        <button id="${show.id}-column-id" type="button"  class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Edit</button>  
+        <button id="${show.id}-column-id-edit" type="button"  class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Edit</button>  
         </td>
         <td>
-        <button id="${show.id}-column-id" type="button"  class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#modal-delete-show">Delete</button>  
+        <button id="${show.id}-column-id-delete" type="button"  class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#modal-delete-show">Delete</button>  
         </td>
         </tr>`)
 
@@ -35,61 +37,56 @@ function showAllShows(data) {
     document.getElementById("tbl-body").innerHTML = sanitizeStringWithTableRows(tableRowsString)
 }
 
-
-function addOne() {
+function addShow() {
+    const htmlId = "-add"
+    optionsForDropdown(htmlId)
     document.getElementById("bnt-submit-show").onclick = makeNewShow
-    function makeNewShow() {
-        const newShow = {}
-        newShow.theater = document.getElementById("input-theater").value
-        newShow.showingTime = document.getElementById("input-showingtime").value
-        newShow.movieID = document.getElementById("input-movie").value
-
-
-        //Now newshow contains all required fields (MUST match the DTO on the backend) and their values
-
-        //Build the options object requred for a POST
-        const options = {}
-        options.method = "POST"
-        options.headers = { "Content-type": "application/json" }
-        options.body = JSON.stringify(newShow)
-
-        fetch(URL, options)
-            .then(r => r.json())
-            .then(addedshow => document.getElementById("returned-new-show").innerText = JSON.stringify(addedshow, null, 2)
-            )
-    }
-    initShows;
 }
+
+function makeNewShow() {
+    const newShow = {}
+    newShow.theater = document.getElementById("select-theater").value
+    newShow.showingTime = document.getElementById("select-time-add").value
+    newShow.movieID = document.getElementById("movie-select-add").value
+    newShow.date = document.getElementById("select-date-add").value
+
+    const options = {}
+    options.method = "POST"
+    options.headers = { "Content-type": "application/json" }
+    options.body = JSON.stringify(newShow)
+
+    fetch(URL, options)
+        .then(r => r.json())
+        .then(addedshow => document.getElementById("returned-new-show").innerText = JSON.stringify(addedshow, null, 2)
+        )
+}
+
 
 function submitEditedShow() {
     const editedShow = {}
     editedShow.id = document.getElementById("id-editshow").value
-    editedShow.theater = document.getElementById("modal-input-theater").value
-    editedShow.showingTime = document.getElementById("modal-input-showingTime").value
-    editedShow.movieID = document.getElementById("modal-input-movie").value
-    //Now newShow contains all required fields (mathces the DTO on the backend) and values
+    editedShow.theater = document.getElementById("select-theater-edit").value
+    editedShow.showingTime = document.getElementById("select-date-edit").value
+    editedShow.movieID = document.getElementById("movie-select-edit").value
 
-    //Build the options object requred for a PUT 
+
     const options = {}
     options.method = "PUT"
     options.headers = { "Content-type": "application/json" }
     options.body = JSON.stringify(editedShow)
-    //Observe, id is added to the URL
+ 
     fetch(URL + editedShow.id + "/" + editedShow.theater + "/" + editedShow.showingTime + "/" + editedShow.movieID, options)
         .then(r => {
             console.log("No Data returned from the server")
             alert("Show was succesfully edited - Not the right way to report this")
         })
-
 }
 
-function deleteShow(){
+function deleteShow() {
     document.getElementById("btn-submit-delete").onclick
     const showToDelete = document.getElementById("id-to-delete").value
-    
-
-    const options ={}
-    options.method="DELETE"
+    const options = {}
+    options.method = "DELETE"
     options.headers = { "Content-type": "application/json" }
     options.body = JSON.stringify(showToDelete)
     fetch(URL + showToDelete, options).then(r => {
@@ -97,22 +94,77 @@ function deleteShow(){
         console.log(showToDelete)
         alert("Show was succesfully deleted - Not the right way to report this")
     })
-    initShows;
+    fetchAllShows;
 }
+
+
 
 function editTarget(evt) {
     const target = evt.target
-    if (!target.id.includes("-column-id")) {
-        return
-    }
-    else {
-        const id = target.id.replace("-column-id", "")
-        document.getElementById("id-editshow").value = id
+    const htmlIdDelete ="-delete"
+    const htmlIdEdit ="-edit"
+
+    if (target.id.includes("-column-id-delete")) {
+        const id = target.id.replace("-column-id-delete", "")
         document.getElementById("id-to-delete").value = id
+        renderShow(id, htmlIdDelete)
+        document.getElementById("btn-submit-delete").onclick = deleteShow
+    }
+     if(target.id.includes("-column-id-edit")){
+        const id = target.id.replace("-column-id-edit", "")
+        document.getElementById("id-editshow").value = id
+        optionsForDropdown(htmlIdEdit)
+        renderShow(id, htmlIdEdit)
         document.getElementById("btn-edited-submit").onclick = submitEditedShow
-        document.getElementById("btn-submit-delete").onclick = deleteShow;
+    }
+
+}
+
+async function optionsForDropdown(htmlId) {
+    const showsValuesForDropdown = await fetch(URL).then(res => res.json())
+    //selectTheaterDropdown(showsValuesForDropdown)
+    selectDateOptions(showsValuesForDropdown, htmlId)
+    selectTimeOptions(showsValuesForDropdown, htmlId)
+    fetchMovieToEdit(htmlId)
+}
+
+
+
+function selectDateOptions(data, htmlId) {
+    const optionsRows = data.map(show => `
+    <option value="${show.date}">${show.date}</option>
+`)
+    document.getElementById("select-date" + htmlId).innerHTML = optionsRows
+
+}
+
+function selectTimeOptions(data, htmlId) {
+    const optionsRows = data.map(show => `
+    <option value="${show.showingTime}">${show.showingTime}</option>
+`)
+    document.getElementById("select-time" + htmlId).innerHTML = optionsRows
+
+}
+
+async function renderShow(id, htmlId) {
+    try {
+        const show = await fetch(URL + id).then(res => res.json())
+        //jsonplaceholder returns an empty object for users not found, NOT an error
+        if (Object.keys(show).length === 0) {  //checks for an empty object = {}
+            throw new Error("No user found for id:" + id)
+        }
+        document.getElementById("show-title" + htmlId).innerText = "Movie title: " + show.movie.title;
+        document.getElementById("show-genre" + htmlId).innerText = "Genre: " + "" + show.movie.genre;
+        document.getElementById("show-theater" + htmlId).innerText = "Theater: " + "" + show.theater;
+        document.getElementById("show-date" + htmlId).innerText = "Date: " + "" + show.date;
+
+    } catch (err) {
+        document.getElementById("error").innerText = err
     }
 }
+
+
+
 
 
 
